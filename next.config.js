e8dd459f73488e1ee8d6898e2143af2a7f98cc43
Luciel Santos <luciel.santos@ubistart.com>
2021-07-06 02:02:19 -0300

    v7

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
+const withPlugins = require('next-compose-plugins');
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const withImages = require('next-images')
 
-module.exports = withImages({
+const withImagesConfig = withImages({
   esModule: true,
 })
+
+const nextConfig = {}
+
+const nextPlugins = [
+  [withImagesConfig],
+  // NOTE: ever in last position
+  [withBundleAnalyzer({})]
+];
+
+module.exports = withPlugins(
+  nextPlugins,
+  nextConfig
+)
