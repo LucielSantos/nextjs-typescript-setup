e81e42e26cfbf99d24adf1de914491c91f6e8dc6
Luciel Santos <luciel.vitor.bs@gmail.com>
2021-07-14 12:14:01 -0300

    add JSDOC on useAPPSelector

--- src/hooks/store/useAppSelector.ts
+++ src/hooks/store/useAppSelector.ts
@@ -1,4 +1,7 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import { IApplicationState } from '../../store';
 
+/**
+ * Typescript abstraction to get typed application state
+ */
 export const useAppSelector: TypedUseSelectorHook<IApplicationState> = useSelector;
