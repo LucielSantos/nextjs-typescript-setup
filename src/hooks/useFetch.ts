03b1796d3f972399c3445e10e1ae76db1f477af2
Luciel Santos <luciel.vitor.bs@gmail.com>
2021-07-14 12:01:07 -0300

    refac useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,12 +1,16 @@
-import useSWR from 'swr';
+import useSWR, { SWRConfiguration } from 'swr';
 import api from '../services/api';
 
-export const useFetch = (url: string) => {
-  const { data, error, mutate } = useSWR(url, async url => {
-    const response = await api.get(url);
+export function useFetch<Data = any, Error = any>(url: string, swrConfig?: SWRConfiguration) {
+  const { data, error, mutate, isValidating, revalidate } = useSWR<Data, Error>(
+    url,
+    async url => {
+      const response = await api.get(url);
 
-    return response.data;
-  });
+      return response.data;
+    },
+    swrConfig,
+  );
 
-  return { data, error, mutate };
-};
+  return { data, error, mutate, isValidating, revalidate };
+}
